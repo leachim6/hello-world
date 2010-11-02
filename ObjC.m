e08fb7a48864dc0f11c87465dc4d46c5e3d2d446
DunkleAura <dunkleaura@gmail.com>
2010-11-02 17:09:33 +0100

    added Objective-C ObjC.m

---
+++ ObjC.m
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello World!");
+	
+    [pool drain];
+    return 0;
+}
