cd65f3c0fff45bb421978020beeef23819a4d7ae
Ivan Kanakarakis <ivan.kanak@gmail.com>
2012-06-21 04:38:39 +0300

    update ruby

--- r/ruby.rb
+++ r/ruby.rb
@@ -1,2 +1,16 @@
 #!/usr/bin/env ruby
-puts "Hello World"
+
+print "Hello World!\n"
+
+# or, using the more common Ruby idiom:
+puts "Hello World!"
+
+# or, for C++ fans:
+$stdout << "Hello World!\n";
+
+# or, for C fans:
+printf("Hello World!\n");
+
+# or, for those who like their output raw:
+p "Hello World!"
+
