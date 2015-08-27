96f48cbcf1bc095a781cc727b5ee4ce2b2befa87
David <davidcondrey@me.com>
2015-08-26 17:39:04 -0700

    cgi written in c

---
+++ c/cgi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+main ()
+{
+   printf ("Content-type: text/html\n");
+   printf ("\n");
+   printf ("<html>\n");
+   printf ("<head>\n");
+   printf ("<title>Hello, world</title>\n");
+   printf ("</head>\n");
+   printf ("\n");
+   printf ("<body>\n");
+   printf ("<h1>Hello, world</h1>\n");
+   printf ("</body>\n");
+   printf ("</html>\n");
+}
