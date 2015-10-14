20d309c4a4bc5aab837a6af0447373bd0811f0c5
Rhed Shi <shi.rhed@gmail.com>
2015-10-14 01:16:00 -0700

    Revert Objective-C back to using NSLog()

--- o/objc.m
+++ o/objc.m
@@ -10,12 +10,5 @@
 
 int main(void)
 {
-    @autoreleasepool
-    {
-        NSFileHandle *_stderr = [NSFileHandle fileHandleWithStandardError];
-        NSString *string = @"hello, world\n";
-        NSData *data = [string dataUsingEncoding:[NSString defaultCStringEncoding]];
-        [_stderr writeData:data];
-    }
-    return EXIT_SUCCESS;
+    NSLog(@"Hello World");
 }
