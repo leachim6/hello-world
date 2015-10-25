8ab92645f09e6de8e11a5e63c66c857b18ae28ac
Shark <shark@krellmachine>
2015-10-25 20:40:48 +0100

    minor changes

--- a/assembler_nasm_linux.asm
+++ a/assembler_nasm_linux.asm
@@ -3,14 +3,15 @@ section	.text
 
 _start:					;tell linker entry point
 
-	mov	edx,len	;message length
-	mov	ecx,msg	;message to write
-	mov	ebx,1	;file descriptor (stdout)
-	mov	eax,4	;system call number (sys_write)
-	int	0x80	;call kernel
+	xor	ebx, ebx
+	lea	edx,[ebx+len]	;message length
+	mov	ecx,msg		;message to write
+	lea	eax,[ebx+4]	;system call number (sys_write)
+	inc	ebx		;file descriptor (stdout)
+	int	0x80		;call kernel
 
-	mov	eax,1	;system call number (sys_exit)
-	int	0x80	;call kernel
+	mov	al,1		;system call number (sys_exit)
+	int	0x80		;call kernel
 
 section	.data
 
