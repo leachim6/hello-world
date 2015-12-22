f571370417557452cf34dd5f78f2ef720b457658
Shark64 <shark@bitchx.it>
2015-12-22 02:16:23 +0100

    align comments

--- a/assembler_nasm_linux.asm
+++ a/assembler_nasm_linux.asm
@@ -8,11 +8,11 @@ _start:					;tell linker entry point
 	lea	edx,[ebx+len]	;message length
 	lea	eax,[ebx+4]	;system call number (sys_write)
 	inc	ebx		;file descriptor (stdout)
-	int	0x80	;call kernel
+	int	0x80		;call kernel
 
-	xor	eax, eax
-	inc	eax	;system call number (sys_exit)
-	int	0x80	;call kernel
+	xor	eax, eax	;set eax=0
+	inc	eax		;system call number (sys_exit)
+	int	0x80		;call kernel
 
 section	.rodata
 
