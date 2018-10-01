83b714bf3029a617eead70c601acedb3ff80c79b
alekhgupta1441 <alekhgupta1441@gmail.com>
2018-10-01 21:54:50 +0530

    Added a file

---
+++ Objective_C/Objective_C_Hello_World!.m
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
