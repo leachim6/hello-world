70f591e877fbfad50b438d2cc9752538d8c5be5d
alekhgupta1441 <alekhgupta1441@gmail.com>
2018-10-01 18:37:29 +0530

    Added C# file

---
+++ C#/Hello World.cs
@@ -0,0 +1,12 @@
+//hello World! program
+namespace HelloWorld
+{
+   class Hello {
+          static void Main(string[] args)
+          {
+          System.Console.WriteLine("Hello World!");
+          }
+
+   }
+
+}
