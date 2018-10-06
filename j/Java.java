9bfb33b5a2d1a5e135d17ddaff524deb37f23860
raunak-mokhasi <40700075+raunak-mokhasi@users.noreply.github.com>
2018-10-07 01:13:07 +0530

    Update Java.java
    The Code shows the Printing of Statements on the same Line and a new Line (System.out.print vs System.out.println)

--- j/Java.java
+++ j/Java.java
@@ -1,5 +1,8 @@
 public class Java {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.print("Hello World"); //Next Statement Prints on the Same Line
+		System.out.println("----------");
+		System.out.println("Hello World"); //Next Statement Prints on the Next Line 
+		System.out.println("----------");
 	}
 }
