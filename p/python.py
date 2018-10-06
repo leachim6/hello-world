35d36ddd07529e51b6901852bd65e4e39db35fb5
raunak-mokhasi <40700075+raunak-mokhasi@users.noreply.github.com>
2018-10-07 01:17:35 +0530

    Update python.py
    Printing statements with and without a newline

--- p/python.py
+++ p/python.py
@@ -1,2 +1,6 @@
 #!/usr/bin/env python
-print "Hello World"
+print("Hello World") #Next Statement prints on the next Line
+print("Hello Universe")
+print()
+print("Hello World", end =" ") #Next Statement prints on the same Line
+print("Hello Universe", end =" ")
