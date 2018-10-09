9981e59436f242eb216cdef7dc8e83a52153ac16
Daniel Kurniadi <daniel.thekurniadi@gmail.com>
2018-10-10 07:01:47 +0800

    Add solidity language for hello world

---
+++ s/solidity.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.4.22;
+contract helloWorld {
+ function renderHelloWorld () public pure returns (string) {
+   return 'Hello World!\n Violet is blue,\n Roses are red,\n My script is Solidity!';
+ }
+}
+~  
