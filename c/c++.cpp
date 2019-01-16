6310de678145324c6884468245c02e36ef113079
Harel Katz <harelka2@gmail.com>
2019-01-16 13:23:54 +0200

    shouldn't use "using namespace std"

--- c/c++.cpp
+++ c/c++.cpp
@@ -1,9 +1,7 @@
 #include <iostream> // include API
 
-using namespace std;
-
 int main() // the main code portion of a C++ program
 {
-   cout << "Hello World" << endl;  //print Hello World on the screen 
+   std::cout << "Hello World" << std::endl;  //print Hello World on the screen 
    return 0; // conventional
 }
