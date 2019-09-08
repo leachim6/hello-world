82ad4138c8ed21f2116489d0449569ef59a72a2f
Anshul Gupta <ag1507anshul@gmail.com>
2019-09-08 19:16:21 +0530

    Add comments on Java.java

--- j/Java.java
+++ j/Java.java
@@ -1,5 +1,8 @@
-public class Java {
+/* File name should be same as class name 
+{Ex- here we use HelloWorld, so file name should be Java.java} */
+class Java {
+	// Your program begins with a call to main()
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World"); //Prints the Hello World to the terminal window
 	}
 }
