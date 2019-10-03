057f900c2357aab0b8fd8355cb20f87d8f2b7e73
LuciferJoshi <56031616+LuciferJoshi@users.noreply.github.com>
2019-10-04 01:45:25 +0530

    Update c.c
    purpose of writing printf should be mentioned along with syntax

--- c/c.c
+++ c/c.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-	printf("Hello World\n");
+	printf("Hello World\n");/*printf is a function stored in <stdio.h> (standard input output) header file and the thing which we need to output is written inside (" ")*/
 	return 0;
 }
