0add08db0c66038b562b329259badd4424e5b730
patilmsangram <43312499+patilmsangram@users.noreply.github.com>
2019-10-25 11:19:32 +0530

    update csharp.cs

--- c/csharp.cs
+++ c/csharp.cs
@@ -1,5 +1,21 @@
-class HelloWorld {
-	static void Main() {
-		System.Console.WriteLine("Hello World");
-	}
-}
+using System; 
+  
+// namespace declaration 
+namespace HelloWorldApp { 
+      
+    // Class declaration 
+    class Geeks { 
+          
+        // Main Method 
+        static void Main(string[] args) { 
+              
+            // statement 
+            // printing Hello World! 
+            Console.WriteLine("Hello World!"); 
+              
+            // To prevents the screen from  
+            // running and closing quickly 
+            Console.ReadKey(); 
+        } 
+    } 
+} 
