eced27abd4fef960c666c31bd2195b3c0d7adf7f
Mohammad Irwansyah S <mirwansyah1933@gmail.com>
2019-10-27 12:45:33 +0700

    Update c++.cpp

--- c/c++.cpp
+++ c/c++.cpp
@@ -1,9 +1,11 @@
 #include <iostream> // include API
+#include <stdio.h> // include API for function printf(), gets(), etc.
 
 using namespace std;
 
 int main() // the main code portion of a C++ program
 {
    cout << "Hello World" << endl;  //print Hello World on the screen 
+   printf("Hello World \n"); //Print Hello World using printf function on the screen
    return 0; // conventional
 }
