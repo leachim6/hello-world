0480316381a389ef3157db73b580740b98d4adf0
Richie Bendall <richiebendall@gmail.com>
2020-03-30 18:48:13 +1300

    Remove unnessesary comments
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- a/arduino.ino
+++ a/arduino.ino
@@ -1,12 +1,8 @@
 void setup() {
-  // put your setup code here, to run once:
-  // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-  //print message serial monitor
   Serial.println("Hello World");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
