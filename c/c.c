0a8f1aa4ce5f0fa428d78b553caabe750b170f7d
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-05-22 00:51:46 +0530

    added comments for more clarification

--- c/c.c
+++ c/c.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
+    //PRINT Hello World , \n is used for the end of the line
 	printf("Hello World\n");
+    //If program is execute successfully , It returns 0 
 	return 0;
 }
