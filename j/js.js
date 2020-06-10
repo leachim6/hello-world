61f4dfd3ba2311aa205955f733dfb2210e17d208
Ritwik Sinha <54446880+Ritwik880@users.noreply.github.com>
2020-06-10 08:45:19 +0530

    Create js.js

---
+++ j/js.js
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>JavaScript Hello World</title>
+</head>
+<body>
+<h1>JavaScript Hello World Example</h1>
+<script>
+<alert(“Hello World!”)
+<script>
+</body>
+</html>
