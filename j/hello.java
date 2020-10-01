bbefdaee9d35c6ea1aece877ee687f73645776d3
Abhishek Nallana <60889353+abhinallana@users.noreply.github.com>
2020-10-01 20:28:11 +0530

    rename to hello.java and added 2 other languages hello world.
     System.out.println("नमस्ते दुनिया");    //hello world in Hindi - Namaste Duniyaa
            System.out.println("ഹലോ വേൾഡ്");       //hello world in malayalam

---
+++ j/hello.java
@@ -0,0 +1,7 @@
+public class Java {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+        System.out.println("नमस्ते दुनिया");    //hello world in Hindi - Namaste Duniyaa
+        System.out.println("ഹലോ വേൾഡ്");       //hello world in malayalam 
+	}
+}
