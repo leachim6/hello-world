e3208b59408ee14a61c909329871dae126d60640
Richie Bendall <richiebendall@gmail.com>
2021-10-17 03:52:02 +1300

    Update ReactNative.js

--- r/ReactNative.js
+++ r/ReactNative.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from "react";
+import { Text, View } from "react-native";
 
 export default function HelloWorld() {
     return (
         <View>
             <Text>Hello World</Text>
         </View>
-    )
+    );
 }
