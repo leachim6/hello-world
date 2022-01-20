b237b158cd267351b6bf630ae48069b6ff0114cb
calgary34 <51468959+calgary34@users.noreply.github.com>
2022-01-20 08:39:30 -0700

    remove comment
    Co-authored-by: Richie Bendall <richiebendall@gmail.com>

--- p/Pygame.py
+++ p/Pygame.py
@@ -4,7 +4,7 @@ from pygame.locals import *
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((400, 300))
 pygame.display.set_caption('Hello World!')
-while True: # main game loop
+while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
