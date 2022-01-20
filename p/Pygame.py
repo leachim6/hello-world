35f45e6f1b820d55627bcbba4f41740296647bd5
calgary34 <51468959+calgary34@users.noreply.github.com>
2022-01-20 09:10:14 -0700

    use double quotes

--- p/Pygame.py
+++ p/Pygame.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('Hello World')
+pygame.display.set_caption("Hello World")
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
