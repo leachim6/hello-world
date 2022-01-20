2ad5684fe5a97e3eed4b682c7fe4726c402b1dbe
calgary34 <51468959+calgary34@users.noreply.github.com>
2022-01-19 22:22:08 -0700

    Create Pygame.py

---
+++ p/Pygame.py
@@ -0,0 +1,12 @@
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((400, 300))
+pygame.display.set_caption('Hello World!')
+while True: # main game loop
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
