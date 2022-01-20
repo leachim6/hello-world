80bdce0f40efff20135debf5ffef1b61fdda3de8
The Peeps191 <74725787+ThePeeps191@users.noreply.github.com>
2022-01-20 14:21:12 -0700

    Create HTTP.py

---
+++ h/HTTP.py
@@ -0,0 +1,14 @@
+import http.server
+import socketserver
+from http import HTTPStatus
+
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(HTTPStatus.OK)
+        self.end_headers()
+        self.wfile.write(b'Hello world')
+
+
+httpd = socketserver.TCPServer(('', 8000), Handler)
+httpd.serve_forever()
