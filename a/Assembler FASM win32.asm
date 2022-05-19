4da68c72729cfed9d720b9f6c77502121211c565
nevernever69 <97673634+nevernever69@users.noreply.github.com>
2022-05-19 11:32:45 +0530

    Add files via upload
    hello world written in FASM

---
+++ a/Assembler FASM win32.asm
@@ -0,0 +1,31 @@
+
+;simple assembly program to write hello world with the help of FASM
+
+
+
+format PE CONSOLE
+entry start
+include 'win32a.inc'
+section '.data' data readable writable
+	helo db "Hello World", 0
+
+section '.text' code readable executable
+start:	xor ecx, ecx
+	push helo
+	call [printf]
+	add esp, 4
+	
+	
+	push ecx
+	call [ExitProcess]
+
+
+
+section '.idata'  import data readable
+library kernel 	, 'kernel32.dll',\
+	msvcrt , 'msvcrt.dll '
+
+import kernel,\
+	ExitProcess, 'ExitProcess'
+import msvcrt,\
+	printf, 'printf'
