aedc20bdd1f8939834bcde61a63d92a47e19ff7f
Kalp Prajapati <81804004+munnokd@users.noreply.github.com>
2023-02-28 16:33:49 +0530

    Update Node.js
    I add server configuration for node js.

--- n/Node.js.js
+++ n/Node.js.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
-console.log('Hello World');
+var http = require('http');
+
+http.createServer(function (req, res) {
+    
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World');
+    
+}).listen(8000);
