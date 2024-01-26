85c6ae535a41cfc84bf9f69381ba367a9c13ba20
CamoCatX <69330490+CamoCatX@users.noreply.github.com>
2024-01-25 19:57:25 -0600

    Update Java.java

--- j/Java.java
+++ j/Java.java
@@ -1,5 +1,6 @@
-public class Java {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+public class hello_world{
+    public static void main (String[] args) {
+        for(long l=4946144450195624l; l>0; l>>=5)
+            System.out.print((char) (((l & 31 | 64) % 95) + 32));
+    }
 }
