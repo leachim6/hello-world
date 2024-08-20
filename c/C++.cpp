ca64eb1534bc392ebda92d16fd893586e979a472
MEBAREK <mojremcodes0001@gmail.com>
2024-08-20 21:24:30 +0100

    added comente in code

--- c/C++.cpp
+++ c/C++.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
+// This is a simple C++ program to print "Hello World"
 int main()
 {
-   std::cout << "Hello World" << std::endl;
+   std::cout << "Hello World" << std::endl;  // Prints the message to the screen
+   return 0;  // Returning 0 indicates successful completion of the program
 }
