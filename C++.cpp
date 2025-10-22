04bdb8db80eabf978bc5666a85df9d22e27a4834
EmiirAcar <166099256+EmiirAcar@users.noreply.github.com>
2025-10-22 09:32:40 +0300

    Add files via upload

---
+++ C++.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    std::cout << "Musa Musakka yemeyi çok sever, afiyet olsun Musa!" << std::endl;
+    return 0;
+}
