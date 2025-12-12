97f2da3cd0826fbc924e2204e8cb1af6ec690f8f
= <=>
2025-12-12 23:26:27 +0000

    i printed my info with javascript with back tick (tempelate litteral)

---
+++ anwar/anwar.js
@@ -0,0 +1,5 @@
+// suitable info and use quokka.js to print this info
+const user = 'anwar tarek';
+const age = 23;
+const job = 'software engineer'
+console.log(`MY NAME IS: ${user}, MY AGE IS: ${age}, MY JOB IS: ${job}`)
